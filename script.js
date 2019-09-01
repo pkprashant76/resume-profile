3ad841939c2acdc80437089403b1f42937a2514a
pkprashant76 <54772707+pkprashant76@users.noreply.github.com>
2019-09-02 03:09:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.querySelector('.container1').addEventListener('click', function(){
+  this.className = (this.className === 'container1 night' ? 'container1' : 'container1 night');
+})
+document.querySelector('.container2').addEventListener('click', function(){
+  this.className = (this.className === 'container2 night' ? 'container2' : 'container2 night');
+})
+document.querySelector('.container3').addEventListener('click', function(){
+  this.className = (this.className === 'container3 night' ? 'container3' : 'container3 night');
+})
+document.querySelector('.container4').addEventListener('click', function(){
+  this.className = (this.className === 'container4 night' ? 'container4' : 'container4 night');
+})
